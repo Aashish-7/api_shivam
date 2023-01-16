b0655a46840f92528a9c58f2f98c94129481f4f3
aashish <aashish.kumar@ioanyt.com>
2023-01-16 14:54:41 +0530

    fix issue

--- src/main/java/com/coforge/api/controller/EmployeeController.java
+++ src/main/java/com/coforge/api/controller/EmployeeController.java
@@ -7,12 +7,11 @@ import com.coforge.api.service.EmployeeService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
+
 
 @RestController
 public class EmployeeController {
